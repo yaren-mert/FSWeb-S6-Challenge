299f30f5271c047a7f3c255051b767f5d9558967
Yaren <yarenmertt96@gmail.com>
2023-01-07 22:46:07 +0300

    projetamam.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {data} from "./mocks/handlers";
+import Karakter from "./components/Karakter";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +9,13 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+const characters = data.map((item,index) => 
+<Karakter key= {index} name= {item.name} birthYear={item.birth_year}/>
+)
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {characters}
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,47 @@
 // Karakter bileşeniniz buraya gelecek
+import React from "react";
+import styled from "styled-components";
+
+
+
+const SCDiv = styled.div`
+display:flex;
+width:400px;
+flex-direction:colmn;
+justify-content: space-between;
+margin: 10px auto;
+border:1px solid darkgreen;
+background: rgba(0, 0, 0, 0.65);
+color:white;
+`
+
+const SCName = styled.p`
+font-weight: bold;
+  letter-spacing: 2px;
+  padding-left: 10px;
+`
+
+const SCBirthYear = styled.p`
+font-weight: bold;
+  letter-spacing: 2px;
+  marjin-right: 10px;
+  border:1px solid white;
+  border-radius:5px
+  
+
+  
+`
+
+
+
+
+export default function Karakter (props){
+const {name,birthYear} = props;
+return(
+<SCDiv>
+    <SCName>{name}</SCName>
+    <SCBirthYear>{birthYear}</SCBirthYear>
+</SCDiv>
+   )
+
+}
